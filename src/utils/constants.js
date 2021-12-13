6a28ce7af217a0356baadd20371cc2911607c0b3
Isnainromadhoni <isnainromadoni@gmail.com>
2021-12-13 23:34:46 +0700

    feat(utils): Add constants.js
    Signed-off-by: Isnainromadhoni <isnainromadoni@gmail.com>

---
+++ src/utils/constants.js
@@ -0,0 +1,35 @@
+export const ANGLE = 360;
+
+export const ALT_ANGLE = 359.99998;
+
+export const ANGLE_DIVISION = 90;
+
+export const NAMESPACE_URI = {
+    HTML: 'http://www.w3.org/1999/xhtml',
+    SVG: 'http://www.w3.org/2000/svg',
+}
+
+export const ARCSHAPE = {
+    UNIFORM: 'uniform',
+    ROUNDED: 'rounded',
+    EDGE: 'edge',
+    REVERSE_EDGE: 'reverse-edge',
+    DOUBLE_EDGE: 'double-edge',
+    REVERSE_DOUBLE_EDGE: 'reverse-double-edge',
+    BEVELLED_EDGE: 'bevelled-edge',
+    UNIFORM_IONTAIL: 'uniform-iontail',
+    UNIFORM_ANTITAIL: 'uniform-antitail',
+    TOP_BEVELLED_IONTAIL: 'top-bevelled-iontail',
+    TOP_BEVELLED_ANTITAIL: 'top-bevelled-antitail',
+    BOTTOM_BEVELLED_IONTAIL: 'bottom-bevelled-iontail',
+    BOTTOM_BEVELLED_ANTITAIL: 'bottom-bevelled-antitail',
+    COMET: 'comet',
+    REVERSE_COMET: 'reverse-comet',
+    METEOR: 'meteor',
+    REVERSE_METEOR: 'reverse-meteor'
+}
+
+export const BEHAVIOR = {
+    GROW: 'grow',
+    SHRINK: 'shrink',
+}
